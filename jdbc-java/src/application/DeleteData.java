a03c9677806e7a02314e5d8ef3418d0ad4b1b5ae
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-05-25 18:59:42 -0300

    transaction

--- jdbc-java/src/application/DeleteData.java
+++ jdbc-java/src/application/DeleteData.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class DeleteData {
 
--- jdbc-java/src/db/DbIntegrityException.java
+++ jdbc-java/src/db/DbIntegrityException.java
@@ -1,4 +1,4 @@
-package application;
+package db;
 
 public class DbIntegrityException extends RuntimeException {
 
